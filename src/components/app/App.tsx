c44ff7a55c44d70517da7cefe677c38251436fad
Erhan Yaşar <mail@erhanyasar.com.tr>
2023-04-20 14:17:31 +0300

    adds default grid layout

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,18 @@
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
+
 function App() {
   return (
-    <div>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2} sx={{ textAlign: 'center' }}>
+        <Grid xs={12} md={6}>
+          component 1
+        </Grid>
+        <Grid xs={12} md={6}>
+          component 2
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
