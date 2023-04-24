fc28cb419ce97a1e4fe85c590b8e8c418b46bc00
Erhan Yaşar <mail@erhanyasar.com.tr>
2023-04-24 22:15:15 +0300

    componentize app.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,14 @@
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';
 import HeroImage from './heroImage';
-import Items from './Items';
+import ItemList from './itemList';
 
 function App() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2} sx={{ textAlign: 'center' }}>
+      <Grid container spacing={2}>
         <HeroImage />
-        <Items />
+        <ItemList />
       </Grid>
     </Box>
   );
