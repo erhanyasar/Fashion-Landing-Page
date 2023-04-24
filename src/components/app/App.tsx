947fcd886f39263f7f56af82fd60e45e0e9f2de3
Erhan Yaşar <mail@erhanyasar.com.tr>
2023-04-24 10:46:02 +0300

    starts adding text content via typography elements

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,15 +1,17 @@
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2} sx={{ textAlign: 'center' }}>
         <Grid xs={12} md={6}>
-          component 1
+            <img src='./img/bg.png' alt="Hero" />
         </Grid>
         <Grid xs={12} md={6}>
-          component 2
+          <Typography variant="h4" component="h2">Everyday items, we have something to suit every occasion.</Typography>
+          <Typography variant="body2" component="span">At suspendisse augue lectus arcu, accumsan ut sit posuere vitae sit tincidunt semper eu proin leo gravida cursus.</Typography>
         </Grid>
       </Grid>
     </Box>
