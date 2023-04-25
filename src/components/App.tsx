a75940895c0b64a514e92fdec7937b89f9cbb003
Erhan Yaşar <mail@erhanyasar.com.tr>
2023-04-25 17:51:45 +0300

    rename main components and apply styling accordingly

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,14 @@
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';
 import HeroImage from './heroImage';
-import ItemList from './itemList';
+import DescriptionList from './descriptionList';
 
 function App() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
+    <Box sx={{ flexGrow: 1, width: '100vw', height: '100vh', overflow: 'hidden' }}>
+      <Grid container spacing={2} sx={{m: 0}}>
         <HeroImage />
-        <ItemList />
+        <DescriptionList />
       </Grid>
     </Box>
   );
