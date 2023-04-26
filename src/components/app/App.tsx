71293270e6efb1059ae73396447cdd8a373cc9ef
Erhan Yaşar <mail@erhanyasar.com.tr>
2023-04-26 16:54:44 +0300

    deletes router instead of creating ambigiouty

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,20 @@
 import { Suspense } from 'react';
-import { CssBaseline, CircularProgress } from '@mui/material';
-import Router from './router';
+import { Box, CssBaseline, CircularProgress } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
+import HeroImage from '../heroImage';
+import DescriptionList from '../descriptionList';
 import '../../fonts/ITCAvantGardePro/stylesheet.css';
 
 function App() {
   return (
     <Suspense fallback={<CircularProgress />}>
       <CssBaseline />
-        <Router />
+      <Box sx={{ flexGrow: 1, width: '100vw', height: '100vh', overflow: 'hidden' }}>
+          <Grid container spacing={2} sx={{m: 0}}>
+            <HeroImage />
+            <DescriptionList />
+          </Grid>
+      </Box>
     </Suspense>
   );
 }
