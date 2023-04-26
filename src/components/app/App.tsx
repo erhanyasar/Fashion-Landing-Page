d84120f092284d3e3fc1fc91908ab91781f5695c
Erhan Yaşar <mail@erhanyasar.com.tr>
2023-04-26 18:13:52 +0300

    create theme and use themeProvider

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,20 +1,27 @@
 import { Suspense } from 'react';
 import { Box, CssBaseline, CircularProgress } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Grid from '@mui/material/Unstable_Grid2';
 import HeroImage from '../heroImage';
 import DescriptionList from '../descriptionList';
+
 import '../../fonts/ITCAvantGardePro/stylesheet.css';
 
 function App() {
+
+  const theme = createTheme();
+
   return (
     <Suspense fallback={<CircularProgress />}>
       <CssBaseline />
-      <Box sx={{ flexGrow: 1, width: '100vw', height: '100vh', overflow: 'hidden' }}>
-          <Grid container spacing={2} sx={{m: 0}}>
-            <HeroImage />
-            <DescriptionList />
-          </Grid>
-      </Box>
+      <ThemeProvider theme={theme}>
+        <Box sx={{ flexGrow: 1, width: '100vw', height: '100vh', overflow: 'hidden scroll' }}>
+            <Grid container spacing={2} sx={{m: 0}}>
+              <HeroImage />
+              <DescriptionList />
+            </Grid>
+        </Box>
+      </ThemeProvider>
     </Suspense>
   );
 }
